5d451c668ef971540c5f2ccda287603105f7d773
slicer <max.schoellhammer01@gmail.com>
2025-03-01 16:29:21 +0100

    added picture to files for first section + tried to add fonts (does not work yet needs to be fixed)

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -13,16 +12,16 @@ function App() {
 
     </head>
 
-<body>
+<body class="">
   <div class="snap-y w-full h-screen overflow-y-scroll snap-mandatory p-0 m-0">
     
-  <div id="section1" class="relative box-border lg:snap-center p-5 md:p-10 lg:h-screen container">
+  <div id="section1" class="relative box-border lg:snap-center p-5 md:p-10 lg:h-screen container bg-[url(https://i.imgur.com/zyqhIFk.png]">
     <h2 class="text-3xl">Section 1</h2>
     <p>Content for section 1...</p>
   </div>
 
   <div id="section2" class="w-full relative box-border lg:h-screen lg:snap-center p-10 bg-gradient-to-b from-[#A27AD3] to-[rgba(0,0,0,0)]">
-    <h2 class="text-4xl text-center">ABOUT</h2>
+    <h1 class="font-\(family-name\:--ClashDisplay-Bold\) text-7xl text-black mb-2">ABOUT</h1>
     <h3 class="text-center text-2xl pt-10">Tauche ein in die spannende Welt von Ryujin, einem actiongeladenen Spiel, bei dem du dich durch endlose Gegnerwellen kämpfen musst. 
       Nutze dein Schwert und Heiltränke, die sich alle 30 Sekunden regenerieren, um am Leben zu bleiben. Jedes besiegte Monster bringt dich dem nächsten Shop näher, 
       wo du deine Ausrüstung verbessern und neue Items kaufen kannst.
