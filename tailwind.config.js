5ad74f3f0369471916a5b53dd25feba6b7c48c9b
SchoellMax <m.schoellhammer@edu.letteverein.berlin>
2025-03-03 15:20:49 +0100

    added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage:{
+        'background': "url('./public/main.png"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
