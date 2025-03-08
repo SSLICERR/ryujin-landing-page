07956b73d2d42f69e0ae75d7de5b55fc72492645
slicer <max.schoellhammer01@gmail.com>
2025-03-08 15:17:20 +0100

    fixed fonts

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
             id="section1"
             class="relative box-border lg:snap-center p-5 md:p-10 lg:h-screen "
           >
-            <div class=" text-w text-center text-3xl ">RYIJUN</div>
+            <div class=" text-w text-center text-3xl font-clash ">RYIJUN</div>
             <div class="text-center">Kämpf dich durch die Wellen!</div>
           </div>
 
@@ -41,13 +41,13 @@ function App() {
                 </div>
 
                 <div className="col-start-1 col-end-2 row-start-2 row-end-3 p-5 border border-myoutline bg-mygray rounded-lg backdrop-blur-md hover:scale-105 transition-transform overflow-hidden">
-                  <p className="text-bermuda text-shadow-[0px_0px_284px_53px_rgba(255,46,227,0.8)]">
+                  <p className="text-bermuda ">
                     Edit <code>src/App.jsx</code> and save to test HMR
                   </p>
                 </div>
 
                 <div className="col-start-2 col-end-4 row-start-2 row-end-3 p-5 border border-myoutline bg-mygray rounded-lg backdrop-blur-lg hover:scale-105 transition-transform">
-                  <p class="text-3xl">
+                  <p class="text-3xl text-bermuda">
                     Jedes besiegte Monster bringt dich dem nächsten Shop näher,
                     wo du deine Ausrüstung verbessern und neue Items kaufen
                     kannst. Entscheide selbst, wann du bereit bist, die nächste
@@ -56,12 +56,8 @@ function App() {
                   </p>
                 </div>
 
-                <div className="col-start-3 col-end-4 row-start-1 row-end-2 p-5 border border-myoutline bg-mygray rounded-lg backdrop-blur-xl hover:scale-105 transition-transform ">
-                  <img
-                    className="w-50 shadow-[0px_0px_193px_0px_rgba(252,252,252,0.8)]"
-                    src={char}
-                    alt=""
-                  />
+                <div className="col-start-3 col-end-4 row-start-1 row-end-2 p-5 border border-myoutline bg-mygray rounded-lg backdrop-blur-xl hover:scale-105 hover:shadow-[0px_0px_193px_0px_rgba(252,252,252,0.8)] transition-transform ">
+                  <img className="w-50 " src={char} alt="" />
                 </div>
               </div>
             </div>
@@ -71,34 +67,36 @@ function App() {
             id="section3"
             class="w-full relative box-border lg:h-screen lg:snap-center p-10"
           >
-            <h2 class="text-3xl ">Section 3</h2>
+            <h2 class="text-3xl font-clash text-mywhite ">TEAM</h2>
             <p>Content for section 3...</p>
             <div className="align-middle self-center justify-center shadow-[0px_0px_184px_73px_rgba(164,60,185,1)]"></div>
           </div>
           <footer class="lg:snap-center h-1/2 box-border w-full relative mt-1/2 bg-black/50 backdrop-blur-sm p-10">
             <div class="md:grid grid-cols-2 w-full text-white">
-              <div class="flex flex-col">
-                <h1 class="font-clash text-4xl mb-2 ">macher.studios</h1>
-                <a class="hover:underline" href="/imprint">
+              <div class="font-ClashDisplayBold flex flex-col">
+                <h1 class="font-clash text-4xl mb-2 text-mywhite">
+                  macher.studios
+                </h1>
+                <a class="hover:underline text-mywhite" href="/imprint">
                   Impressum
                 </a>
-                <a class="hover:underline" href="/privacy">
+                <a class="hover:underline text-mywhite" href="/privacy">
                   Datenschutz
                 </a>
               </div>
               <div class="flex flex-col">
-                <h1 class="font-clash text-4xl mb-2">Kontakt</h1>
+                <h1 class="font-clash text-4xl mb-2 text-mywhite">Kontakt</h1>
                 <a
                   href="mailto:m.schoellhammer@edu.letteverein.berlin"
                   target="_blank"
                   rel="noopener noreferrer"
-                  class="hover:underline"
+                  class="hover:underline text-mywhite"
                 >
                   E-Mail
                 </a>
               </div>
             </div>
-            <div class="absolute bottom-10 left-10">
+            <div class="absolute bottom-10 left-10 text-mywhite">
               © Copyright macher.studios 2025 - All rights reserved.
             </div>
           </footer>
