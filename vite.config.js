fd6d379490b4be175e324eeeff009d22a1ba1663
sslicerr <max.schoellhammer01@gmail.com>
2025-03-26 18:05:47 +0100

    update to base url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ryujin-landing-page",
+  base: "/ryujin-landing-page/",
   plugins: [react(),tailwindcss(),],
 
    
