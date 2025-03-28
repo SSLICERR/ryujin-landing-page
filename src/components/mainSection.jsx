de7ac60fefaec258bbaa625cf1c1df6ad5b2f364
sslicerr <max.schoellhammer01@gmail.com>
2025-03-28 22:51:57 +0100

    added the logo instead of normal writing for main section

--- src/components/mainSection.jsx
+++ src/components/mainSection.jsx
@@ -2,59 +2,60 @@ import React from "react";
 import { SiInstagram, SiX, SiYoutube } from "react-icons/si";
 import { SiSteam } from "react-icons/si";
 import { IoIosArrowDown } from "react-icons/io";
+import logosvg from "../assets/logosvg.svg";
 
-const MainSection = () =>{
-   return(
+const MainSection = () => {
+  return (
     <>
-    <div
-            id="section1"
-            class="relative box-border lg:snap-center p-5 md:p-10 lg:h-screen "
-          >
-            <div class=" text-w text-center text-6xl font-clashB tracking-widest">
-              RYUJIN
-            </div>
+      <div
+        id="section1"
+        class="relative box-border lg:snap-center p-5 md:p-10 lg:h-screen "
+      >
+        <div className="flex justify-center items-center">
+          <img src={logosvg} alt="" className="w-70" />
+        </div>
 
-            <div class="text-center font-clashM">
-              Kämpf dich durch die Wellen!
-            </div>
-            <div className="p-20 text-center">
-              <button className="btn btn-neutral" href="/">Join Waitlist</button>
-            </div>
-            <div className="text-center mt-100">
-              <button
-                className=" cursor-pointer p-2"
-                onClick="location.href='http://twitter.com"
-              >
-                <SiInstagram color="black" size={30} />
-              </button>
-              <button
-                className=" cursor-pointer p-2"
-                onClick="location.href='http://twitter.com"
-              >
-                <SiSteam color="black" size={30} />
-              </button>
-              <button
-                className=" cursor-pointer p-2"
-                onClick="location.href='http://twitter.com"
-              >
-                <SiX color="black" size={30} />
-              </button>
-              <button
-                className=" cursor-pointer p-2"
-                onClick="location.href='http://twitter.com"
-              >
-                <SiYoutube color="black" size={30} />
-              </button>
-              <div className="flex items-center justify-center pt-10">
-                <IoIosArrowDown
-                  className="animate-bounce "
-                  size={40}
-                  color="black"
-                />
-              </div>
-            </div>
+        
+        <div className="p-20 text-center">
+        <button className="btn btn-neutral" href="/">
+            Join Waitlist
+          </button>
+        </div>
+        <div className="text-center mt-100">
+          <button
+            className=" cursor-pointer p-2"
+            onClick="location.href='http://twitter.com"
+          >
+            <SiInstagram color="black" size={30} />
+          </button>
+          <button
+            className=" cursor-pointer p-2"
+            onClick="location.href='http://twitter.com"
+          >
+            <SiSteam color="black" size={30} />
+          </button>
+          <button
+            className=" cursor-pointer p-2"
+            onClick="location.href='http://twitter.com"
+          >
+            <SiX color="black" size={30} />
+          </button>
+          <button
+            className=" cursor-pointer p-2"
+            onClick="location.href='http://twitter.com"
+          >
+            <SiYoutube color="black" size={30} />
+          </button>
+          <div className="flex items-center justify-center pt-10">
+            <IoIosArrowDown
+              className="animate-bounce "
+              size={40}
+              color="black"
+            />
           </div>
+        </div>
+      </div>
     </>
-   ); 
+  );
 };
 export default MainSection;
