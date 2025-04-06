b957ec294e2a474f87a7b55075bbfb2cdb7a2412
slicer <max.schoellhammer01@gmail.com>
2025-04-06 17:46:02 +0200

    refactor MainSection: update class attributes to className, modify button actions for social links

--- src/components/mainSection.jsx
+++ src/components/mainSection.jsx
@@ -9,44 +9,44 @@ const MainSection = () => {
     <>
       <div
         id="section1"
-        class="relative box-border lg:snap-center p-5 md:p-10 lg:h-screen "
+        className="relative box-border lg:snap-center p-5 md:p-10 lg:h-screen "
       >
         <div className="flex justify-center items-center">
           <img src={logosvg} alt="" className="w-70" />
         </div>
         <div className="p-20 text-center">
-          <button className="btn btn-neutral" href="/">
-            JOIN WAITLIST
-          </button>
+          <button className="btn btn-neutral">JOIN WAITLIST</button>
         </div>
         <div className="text-center mt-100">
           <button
-            className=" cursor-pointer p-2"
-            onClick="location.href='http://twitter.com"
+            className="cursor-pointer p-2"
+            onClick={() => window.open("https://instagram.com", "_blank")}
           >
             <SiInstagram color="black" size={30} />
           </button>
           <button
-            className=" cursor-pointer p-2"
-            onClick="location.href='http://twitter.com"
+            className="cursor-pointer p-2"
+            onClick={() =>
+              window.open("https://store.steampowered.com", "_blank")
+            }
           >
             <SiSteam color="black" size={30} />
           </button>
           <button
-            className=" cursor-pointer p-2"
-            onClick="location.href='http://twitter.com"
+            className="cursor-pointer p-2"
+            onClick={() => window.open("https://twitter.com", "_blank")}
           >
             <SiX color="black" size={30} />
           </button>
           <button
-            className=" cursor-pointer p-2"
-            onClick="location.href='http://twitter.com"
+            className="cursor-pointer p-2"
+            onClick={() => window.open("https://youtube.com", "_blank")}
           >
             <SiYoutube color="black" size={30} />
           </button>
           <div className="flex items-center justify-center pt-10">
             <IoIosArrowDown
-              className="animate-bounce "
+              className="animate-bounce"
               size={40}
               color="black"
             />
