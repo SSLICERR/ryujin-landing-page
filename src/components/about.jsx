3438b3d3095bc681e3d4b0eddb4edb7bee2508d2
slicer <max.schoellhammer01@gmail.com>
2025-04-06 15:56:29 +0200

    added new image asset and updated AboutSection with new content and layout adjustments

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import char from "../assets/char.gif";
+import char2 from "../assets/fertgig.png";
 
 const AboutSection = () => (
   <>
@@ -12,7 +13,7 @@ const AboutSection = () => (
     <div className="flex justify-center items-center min-h-screen ">
       <div className="grid grid-cols-2 grid-rows-3 gap-5 gap-y-15 p-5 w-300 h-250">
         <div className="col-start-1 col-end-3 row-start-1 row-end-2 p-5 border border-myoutline bg-mygray rounded-lg backdrop-blur-sm transition-transform">
-          <p className="text-2xl text-mywhite font-clashR">
+          <p className="text-2xl text-mywhite font-clashR text-center">
             Tauche ein in die spannende Welt von Ryujin, einem actiongeladenen
             Spiel, bei dem du dich durch endlose Gegnerwellen kämpfen musst.
             Nutze dein Schwert und Heiltränke, die sich alle 30 Sekunden
@@ -21,17 +22,13 @@ const AboutSection = () => (
         </div>
 
         <div className="col-start-1 col-end-2 row-start-2 row-end-3 p-5 border border-myoutline bg-mygray rounded-lg backdrop-blur-md transition-transform overflow-hidden">
-          <p className="text-mywhite font-clashR">
-            hier kommt noch nen Bild oder so hin
-          </p>
+          <img className="w-100" src={char2} alt="" />
         </div>
 
         <div className="col-start-2 col-end-4 row-start-2 row-end-3 p-5 border border-myoutline bg-mygray rounded-lg backdrop-blur-lg transition-transform">
-          <p className="text-2xl text-mywhite font-clashR">
-            Jedes besiegte Monster bringt dich dem nächsten Shop näher, wo du
-            deine Ausrüstung verbessern und neue Items kaufen kannst. Entscheide
-            selbst, wann du bereit bist, die nächste Welle zu starten, und
-            bereite dich auf den ultimativen Bosskampf vor.
+          <p className="text-2xl text-mywhite font-clashR text-center">
+            Sammle Goldmünzen um neue Fähigkeiten freizuschalten und Gegenstände
+            zu kaufen, um noch mächtiger zu werden.
           </p>
         </div>
 
@@ -41,16 +38,16 @@ const AboutSection = () => (
 
         {/* New row starts here */}
         <div className="col-start-1 col-end-3 row-start-3 row-end-4 p-5 border border-myoutline bg-mygray rounded-lg backdrop-blur-md transition-transform">
-          <p className="text-2xl text-mywhite font-clashR">
-            Sammle Erfahrungspunkte und schalte neue Fähigkeiten frei, um noch
-            mächtiger zu werden.
+          <p className="text-2xl text-mywhite font-clashR text-center">
+            Jedes besiegte Monster bringt dich dem nächsten Shop näher, wo du
+            deine Ausrüstung verbessern und neue Items kaufen kannst. Entscheide
+            selbst, wann du bereit bist, die nächste Welle zu starten, und
+            bereite dich auf den ultimativen Bosskampf vor.
           </p>
         </div>
 
         <div className="col-start-3 col-end-4 row-start-3 row-end-4 p-5 border border-myoutline bg-mygray rounded-lg backdrop-blur-lg transition-transform">
-          <p className="text-2xl text-mywhite font-clashR">
-            Bild
-          </p>
+          <p className="text-2xl text-mywhite font-clashR">Bild</p>
         </div>
       </div>
     </div>
